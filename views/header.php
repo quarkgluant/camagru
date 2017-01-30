bf4b19281ab47f82b222c2c8eea7a00c5257d265
Franck SANGARE <fsangare@e1r11p19.42.fr>
2017-01-30 17:04:15 +0100

    camagru main_camagu.php

---
+++ views/header.php
@@ -0,0 +1,17 @@
+<header>
+  <center><h1><B> Vous êtes sur Camagru</B></h1></center>
+  <br/>
+  <nav>
+    <ul>
+      <li><a href="#">Navigation</a></li>
+      <li><a href="#">Qui nous sommes</a></li>
+      <li><a href="#">Ce que nous faisons</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+
+    <form>
+      <input type="search" name="q" placeholder="Recherchez...">
+      <input type="submit" value="OK">
+    </form>
+  </nav>
+</header>
