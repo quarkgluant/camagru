9526838af5179c6999f4bd21b7f289847bc83003
Patrice CADIOT <pcadiot@e1r12p18.42.fr>
2017-02-10 20:42:00 +0100

    crée 2 nouveaux fichiers modif password

---
+++ app/views/main_user_password_modify.php
@@ -0,0 +1,43 @@
+
+  <td>
+        <br/>
+        <p><B><I>Modification de votre mot de passe :</I></B></p>
+        <br/>
+        <br/>
+<?php
+	include __DIR__ . '../../src/password_modify.php';
+?>
+    <table>
+          <tr>
+            <td>
+                <form method="post">
+                Pseudo.........................: <input type="text" name="login" value=""/>
+            </td>
+            <td>
+                Mail....................................................: <input type="email" name="email" value=""/>
+            </td>
+            <td>
+                Ancien mot de passe: <input type="password" name="password" value=""/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Nouveau mot de passe: <input type="password" name="passwordnew1" value=""/>
+            </td>
+            <td>
+            Vérification du nouveau mot de passe: <input type="password" name="passnewverif" value=""/>
+            </td>
+            <td>
+            </td>
+        </tr>
+    </table>
+                    <input type="submit" name="submit" value="OK" />
+                    <INPUT TYPE="reset" NAME="reset" VALUE="Effacer">
+                    </form>
+
+                <br/>
+                <br/>
+                <br/>
+                <i><a  href = "./main_camagru.php"> Revenir à la page précédente !!!! </a></i>
+                <br/>
+                <br/>
