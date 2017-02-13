78a7b48bfb2b875b2ef28c88d0e34b6de477693c
Patrice CADIOT <pcadiot@e1r12p18.42.fr>
2017-02-13 15:01:45 +0100

    functions php d'initialisation de la BDD fonctionnent

--- app/config/database.php
+++ app/config/database.php
@@ -1,5 +1,5 @@
 <?php
 $DB_NAME = 'camagru';
-$DSN = "mysql:host=localhost;dbname={$DB_NAME};charset=utf8";
+$DB_DSN = "mysql:host=localhost;dbname={$DB_NAME};charset=utf8";
 $DB_USER = 'root';
-$DB_PASS = 'root';
+$DB_PASSWORD = 'root';
