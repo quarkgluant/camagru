db79626a9cf6351fd9f85a0838bcc8d4307ac910
Patrice CADIOT <pcadiot@e1r11p17.42.fr>
2017-02-14 17:25:38 +0100

    rajoute des constantes dans config/setup.php

--- app/config/setup.php
+++ app/config/setup.php
@@ -1,4 +1,15 @@
 <?php
+
+// Identifiants pour la base de données. Nécessaires a PDO.
+define('SQL_DSN',      'mysql:dbname=camagru;host=localhost');
+define('SQL_USERNAME', 'root');
+define('SQL_PASSWORD', 'root');
+
+// Chemins à utiliser pour accéder aux vues/modeles/librairies
+// $module = empty($module) ? !empty($_GET['module']) ? $_GET['module'] : 'index' : $module;
+define('CHEMIN_VUE',    'app/src/views/');
+define('CHEMIN_MODELE', 'app/src/modeles/');
+
 require_once('database.php');
 $db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
 $sql = file_get_contents('base.sql');
