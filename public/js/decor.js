2e1890f2ff227446eb8b793a465f199d6fb92229
Patrice CADIOT <pcadiot@e1r12p17.42.fr>
2017-02-28 15:27:29 +0100

    modifie fonction decor.js

--- public/js/decor.js
+++ public/js/decor.js
@@ -1,8 +1,8 @@
 function decor() {
-  select = document.getElementById("select");
+  select = document.getElementById("decor");
   choice = select.selectedIndex  // Récupération de l'index du <option> choisi
   valeur_cherchee = select.options[choice].value; // Récupération de la valeur du <option> choisi
-  img = "imgtag2";// Nom de l'image
+  img = document.getElementById("imgtag2");// Nom de l'image
   img.src = valeur_cherchee;
-    var r = confirm(valeur_cherchee);
+  var r = confirm(valeur_cherchee);
 }
