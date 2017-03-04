e8bf55c4401b6046b2da7ca1b4411ed0b580ffaf
Patrice Cadiot <pathibul.r@gmail.com>
2017-03-04 18:24:13 +0100

    ajoute temporisation de sortie dans user_create afin de remédier au problème de Header already send

--- app/src/user_create.php
+++ app/src/user_create.php
@@ -1,4 +1,6 @@
 <?php
+ob_start();
+
 session_start();
 
 include_once __DIR__ . "/modeles/membres.php";
@@ -9,23 +11,10 @@ $message = identificationUser();
 
 if ($message == '1')
 {
-    $url = "file://". __DIR__ . "/views/main_camagru.php";
-    if(!headers_sent()) {
-        //If headers not sent yet... then do php redirect
-        header('Location: '.$url);
-        exit();
-    }
-    else
-    {
-        //If headers are sent... do javascript redirect... if javascript disabled, do html redirect.
-        echo '<script type="text/javascript">';
-        echo 'window.location.href="'.$url.'";';
-        echo '</script>';
-        echo '<noscript>';
-        echo '<meta http-equiv="refresh" content="0;url='.$url.'" />';
-        echo '</noscript>';
-        exit;
-    }
+    header('Location: ./views/main_camagru.php');
+    exit();
 }
 else
   include_once __DIR__ . "/views/messages.php";
+
+ echo ob_get_clean();
