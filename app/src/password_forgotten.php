0f41342a4500adb5d6c325a118ffe8209e1be47d
Patrice Cadiot <pathibul.r@gmail.com>
2017-03-09 17:19:00 +0100

    remets les 2 fichiers password_forgotten

---
+++ app/src/password_forgotten.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+include_once __DIR__ . "/modeles/membres.php";
+include_once __DIR__ . "/controleur/controleur.php";
+include_once __DIR__ . "/views/password_forgotten.php";
+// $destinataire = 'pcadiot@gmail.com';
+// Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
+$expediteur = 'pathibul.r@gmail.com';
+//$copie_cachee = 'pcadiot@student.42.fr';
+$objet = "reinitialiser le mot de passe"; // Objet du message
+$headers = 'MIME-Version: 1.0' . "\n"; // Version MIME
+$headers .= 'Content-type: text/html; charset=ISO-8859-1' . "\n"; // l'en-tete Content-type pour le format HTML
+$headers .= 'Reply-To: ' . $expediteur . "\n"; // Mail de reponse
+$headers .= 'From: "Camagru contact"<' . $expediteur . '>' . "\n"; // Expediteur
+$headers .= 'Delivered-to: ' . $destinataire . "\n"; // Destinataire
+// $headers .= 'Cc: ' . $copie . "\n"; // Copie Cc
+// $headers .= 'Bcc: ' . $copie_cachee . "\n\n"; // Copie cachée Bcc
+$message =  "<a href='http://localhost:8080/base/app/src/views/main_user_password_modify.php'>cliquez ici</a>";
+if (isset($_POST['login'], $_POST['email'])) {
+    $_SESSION['loggued_on_user'] = $_POST['login'];
+    $_SESSION['passwd_hash'] = getPassword($_POST);
+    $destinataire = $_POST['email'];
+    $copie = 'pathibul.r@gmail.com';
+    if (mail($destinataire, $objet, $message, $headers)){
+    ?>
+
+    </br>
+    <H5 style="color: green; text-align:center; font-style : bold ; font-family : monospace ; box-shadow:0 0 10px;">
+        Mail correctement envoyé au format HTML !</h5>
+    </br>
+
+    <?php
+    }// Envoi du message
+    else {// Non envoyé
+        ?>
+
+        </br>
+        <H4>Problème d'envoi du mail au format HTML !</H4>
+        </br>
+
+        <?php
+    }
+}
+$_POST["mail"] = "";
+$_POST["msg"] = "";
+$_POST["obj"] = "";
+$_POST["html"] = "txt";
+$_POST["msg"] = "";
+?>
---
+++ app/src/views/password_forgotten.php
@@ -0,0 +1,37 @@
+<html>
+<HEAD>
+    <title id="title-doc">Camagru</title>
+    <meta content="camagru; sangare; cadiot; 42; école 42; php" name="keywords">
+    <Meta charset="UTF-8">
+    <link rel="stylesheet" href="../../public/css/application.css"/>
+
+</head>
+
+<body>
+<center><B><h1>Ouvrez vous à la magie de Camagru !</h1></B></center>
+<br/>
+<br/>
+<br/>
+<p><B><I>Réinitialiser votre mot de passe :</I></B></p>
+<br/>
+<br/>
+<form method="post" action="../src/password_forgotten.php">
+    Pseudo : <input type="text" name="login" placeholder="login" required/>
+    Mail : <input type="email" name="email" placeholder="votre mail" required
+                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"/>
+    <input type="submit" name="submit" value="OK"/>
+    <INPUT TYPE="reset" NAME="reset" VALUE="Effacer">
+</form>
+<br/>
+
+<br/>
+<br/>
+<a href="../../public/index.php"> Revenir à l'accueil !!!! </a>
+<br/>
+<br/>
+<br/>
+<hr/>
+<br/>
+<p class="copyright"> © Camagru - pcadiot/fsangare 2017 </p>
+</body>
+</html>
