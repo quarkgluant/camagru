4fabaec658bdae0a5fccbf8440956b0b7b920e11
franck Sangare <fsangare@student.42.fr>
2017-03-21 11:32:07 +0100

    Envoi mail pour commentaire photo

--- app/src/add_comments.php
+++ app/src/add_comments.php
@@ -6,7 +6,7 @@ include_once __DIR__ . "/controleur/controleur.php";
 $commentaire = array('login' => $_SESSION['loggued_on_user'], 'comment' => $_POST['comment'], 'img_id' => $_POST['image_hidden']);
 $message = sauvegarde_review($commentaire);
 if ($message == "xxxxxxxxxx"){
-include_once __DIR__ . "comments_mail.php";
+  include_once __DIR__ . "comments_mail.php";
 }
 header("Location: ../../app/src/views/main_camagru.php");
 exit();
