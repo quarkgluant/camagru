5cacee698be44df4e60527b1b121b4aca8dcda98
Patrice Cadiot <pathibul.r@gmail.com>
2017-03-23 13:56:52 +0100

    reformate esthétiquement une chaine dans side2.php

--- app/src/views/side2.php
+++ app/src/views/side2.php
@@ -6,6 +6,8 @@
 
     $dossier_ref = '../../../public/img/save/';
     $dossier_heart = '../../../public/img/heart/';
+    // ici on doit appeler la/les fonctions du controleur pour peupler les images
+    // sauvegardées dans la base
     if (($dossier = opendir('../../../public/img/save')))
     {
         while (false !== ($fichier = readdir($dossier)))
@@ -13,9 +15,13 @@
             //if ($fichier != '.' && $fichier != '..' && $fichier != 'index.php' && !is_dir($fichier))
           if (strstr($fichier, '.png') || strstr($fichier, '.jpg') || strstr($fichier, '.jpeg') || strstr($fichier, '.gif'))
             {
-                $nb_fichier++; // On incrémente le compteur de 1
+                $nb_fichier++;
                 $nb_heart = rand(0, 10);
-                echo '&nbsp;&nbsp;<A href="./main_comments.php?image='. $fichier .'"><img id="imgtag4" src="./' . $dossier_ref . $fichier . '"width="75" height="75" style="border: 1px solid black;" title="Cliquez pour accéder aux commentaires"/></A>&nbsp;'.$nb_heart.'&nbsp;<A href="#"><img src=" ./' . $dossier_heart .'1.png" width="12" height="12"></A></BR>';
+                echo '&nbsp;&nbsp;<A href="./main_comments.php?image='. $fichier
+                    .'"><img id="imgtag4" src="./' . $dossier_ref . $fichier
+                    . '"width="75" height="75" style="border: 1px solid black;" title="Cliquez pour accéder aux commentaires"/></A>&nbsp;'
+                    . $nb_heart .'&nbsp;<A href="#"><img src=" ./'
+                    . $dossier_heart .'1.png" width="12" height="12"></A></BR>';
                 if ($nb_fichier%10 == 0)
                   {
                       echo '</td><td></br></br>';
